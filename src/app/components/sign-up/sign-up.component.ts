efff3a1934f8f64c6b0a8bd560464ccde5aaabd8
tzipi-neugrashl <tzipi05567@gmail.com>
2025-06-08 01:33:40 +0300

    'moreCommit'

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { StudentsService } from '../../services/students.service';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
+import { AgGridAngular } from 'ag-grid-angular';
+import type { ColDef } from 'ag-grid-community';
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
@@ -12,4 +16,12 @@ export class SignUpComponent {
   constructor(public studentService: StudentsService){}
 
   studentList = this.studentService.getStudents();
+  columnDefs: ColDef[] = [
+    { field: 'name', headerName: 'Name', sortable: true, filter: true },
+    { field: 'phone', headerName: 'Number Phone', sortable: true, filter: true },
+    { field: 'lessonName', headerName: 'Course', sortable: true, filter: true },
+    { field: 'paid', headerName: 'Paid', sortable: true, filter: true }
+  ];
+
+
 }
